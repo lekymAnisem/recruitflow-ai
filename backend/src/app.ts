d921d262de9aee00f23998de6f9da74ef44807b4
lekymAnisem <deidei0813@gmail.com>
2026-06-28 08:25:55 +0800

    Allow CORS for additional IP address

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,7 +25,7 @@ const app = express();
 app.use(
   cors({
     origin(origin, callback) {
-      if (!origin || /^https?:\/\/localhost(:\d+)?$/.test(origin)) {
+      if (!origin || /^https?:\/\/(localhost|13\.211\.245\.10)(:\d+)?$/.test(origin)) {
         callback(null, true);
       } else {
         callback(new Error(`Origin ${origin} not allowed by CORS`));
